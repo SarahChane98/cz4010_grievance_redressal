06ee8c6c8ec9b605db082da0cd5c4ca99b5e25ee
Yulin Zou <zouy0013@e.ntu.edu.sg>
2021-11-28 21:40:15 +0800

    add new attribute

--- posts/models.py
+++ posts/models.py
@@ -13,6 +13,9 @@ class Post(models.Model):
     is_resolved = models.BooleanField(default=False)
     is_read = models.BooleanField(default=False)
     reply_by_authority = models.TextField(blank=True, default='')
+    post_sig = models.TextField()
+    reply_sig = models.TextField()
+    ring_members = models.TextField()
 
     def __str__(self):
         return self.title
